9dd868a6a80245ae6c9eac8895e4fd3995e639be
terebator228 <mish.8@mail.ru>
2022-07-14 19:41:23 +0300

    made obj personlMovieDB

--- alertJS.js
+++ alertJS.js
@@ -1,3 +1,15 @@
 
+'use strict';
+
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0 )
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: [],
+  actors: [],
+  genres: [],
+  privat: false,
+};
+
+console.log(personalMovieDB);
  
