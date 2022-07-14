0787cca54fe0ecc6a2f40d99b75916a2ff1107cd
terebator228 <mish.8@mail.ru>
2022-07-14 19:33:30 +0300

    add numberOfFilms

--- alertJS.js
+++ alertJS.js
@@ -1,16 +1,3 @@
 
-function unique(arr) {
-   let sort = new Set();
-   for (let user of values) {
-      sort.add(user);
-   }
-   console.log(sort);
- }
-
-
- let values = ["Hare", "Krishna", "Hare", "Krishna",
-   "Krishna", "Krishna", "Hare", "Hare", ":-O"
- ];
-
- alert(unique(values));
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0 )
  
