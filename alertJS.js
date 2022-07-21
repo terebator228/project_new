a4f084ac8b7035365bdb66ca9c9e904cf895ca82
terebator228 <mish.8@mail.ru>
2022-07-21 13:34:32 +0300

    Удалил неиспользуемый код

--- alertJS.js
+++ alertJS.js
@@ -72,32 +72,3 @@ function showMyDB(privat) {
 
 showMyDB(personalMovieDB.privat);
 
-/*while (numberOfFilms == '' || !numberOfFilms) {
-    numberOfFilms = +prompt('Сколько фильмво вы просмотрели?', '');
-};
-
-while (a.length >= 50 || !a ) {
-    a = prompt('Какой из последних фильмов вы смотрели?', '');
-};
-
-while (b.length >= 50 || !b ) {
-    b = +prompt('На сколько баллов вы его оценили?', '');
-};
-
-while (c.length >= 50 || !c ) {
-    c = prompt('Какой из последних фильмов вы смотрели?', '');
-}
-
-while (d.length >= 50 || !d ) {
-    d = +prompt('На сколько баллов вы его оценили?', '');
-}
-
-
-
-if(personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов", '')
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert("Вы классический зритель", '');
-} else if(personalMovieDB.count > 30) {
-    alert("Вы киноман", '');
-}; */
